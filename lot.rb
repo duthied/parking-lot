a3996177f06e4964ac56a3a5b06592cbd69bf85d
Devlon <devlon@Devlons-MacBook-Pro.local>
2023-04-01 16:56:18 -0400

    initial commit

---
+++ lot.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+class Lot
+
+  def initialize(max_capacity = 5)
+    @max_cap = max_capacity
+    puts "\nSalem's (Parking) Lot open for business\noffering #{@max_cap} luscious parking spots\n\n"
+  end
+
+end
