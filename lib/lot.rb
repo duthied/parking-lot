13cd918a2e25d5718f83282e4f82cbb05e372ada
Devlon Duthie <duthied@duthied-mbp-2019.local>
2023-04-02 13:18:48 -0400

    Tidy

--- lib/lot.rb
+++ lib/lot.rb
@@ -1,14 +1,13 @@
 #!/usr/bin/env ruby
 require 'json'
 
-ParkingPass = Struct.new(:time, :plate_number)
+ParkingPass = Struct.new(:plate_number, :time)
 
 class Lot
 
   def initialize(max_capacity = 10)
     @max_capacity = max_capacity
     @parked_cars = {}
-    # puts "\nSalem's (Parking) Lot open for business\noffering #{@max_capacity} luscious parking spots\n\n"
   end
 
   def vacant_space_count
@@ -19,12 +18,18 @@ class Lot
     return @parked_cars.size < @max_capacity
   end
 
+  def list_cars
+    @parked_cars.each do | key, value |
+      puts "#{key}"
+    end
+  end
+
   # takes a plate number and if there is capacity
   # returns a parking pass with the current timestamp
   def park_car(plate_number)
     return nil unless @parked_cars.size < @max_capacity
 
-    pp = ParkingPass.new Time.now(), plate_number # .to_json
+    pp = ParkingPass.new plate_number, Time.now()  # .to_json
     
     # add new parking pass to the hash
     @parked_cars[plate_number] = pp.to_json
