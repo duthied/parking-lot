800981414fdffba6c762abafa3a6a68eb45e03df
Devlon Duthie <duthied@duthied-mbp-2019.local>
2023-04-02 11:17:29 -0400

    Adds basic first test to make sure all is setup

--- spec/lot_spec.rb
+++ spec/lot_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 require_relative '../lib/lot'
 
+describe 'lot' do
+  it 'should initialize' do
+    expect(Lot).to receive(:new)
+    obj = Lot.new
+  end 
+end
+
 # describe 'addOne' do
 #   it 'should add 1 to what was passed in' do
 #     expect(addOne(1)).to eq 2
